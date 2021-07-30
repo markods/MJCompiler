ff03fff21b385bf7a8b9592d8809d08cfbcd2ae9
Marko Stanojević <marko.d.stanojevic@gmail.com>
2021-07-30 15:13:20 +0200

    popravljena cup run komanda

--- cup.ps1
+++ cup.ps1
@@ -1,3 +1,5 @@
-$CompilerCmd = "java -cp '.\MJCompiler\lib\cup_v10k.jar' java_cup.Main -destdir 'rs\ac\bg\etf\pp1' -dump_states -parser 'MJParser' -ast 'rs.ac.bg.etf.pp1.ast' -buildtree '..\spec\mjparser.cup'"
+$CompilerCmd = "java -cp './MJCompiler/lib/cup_v10k.jar' java_cup.Main -destdir './MJCompiler/src/rs/ac/bg/etf/pp1' -parser 'MJParser' -ast ast -buildtree './MJCompiler/spec/mjparser.cup'"
 $CompilerCmd
 Invoke-Expression -Command $CompilerCmd
+
+# dump_states
