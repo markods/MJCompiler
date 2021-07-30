b01fec25f1e008a269157791eaf15801f94974b8
Marko Stanojević <marko.d.stanojevic@gmail.com>
2021-07-30 17:28:51 +0200

    dodat source kod biblioteka

--- cup.ps1
+++ cup.ps1
@@ -1,5 +1,3 @@
-$CompilerCmd = "java -cp './MJCompiler/lib/cup_v10k.jar' java_cup.Main -destdir './MJCompiler/src/rs/ac/bg/etf/pp1' -parser 'MJParser' -ast ast -buildtree './MJCompiler/spec/mjparser.cup'"
+$CompilerCmd = "java -cp './MJCompiler/lib/cup_v10k.jar' java_cup.Main -dump_states -destdir './MJCompiler/src/rs/ac/bg/etf/pp1' -parser 'MJParser' -ast 'MJCompiler/src/rs/ac/bg/etf/pp1/ast' -buildtree './MJCompiler/spec/mjparser.cup'"
 $CompilerCmd
 Invoke-Expression -Command $CompilerCmd
-
-# dump_states
--- jflex.ps1
+++ jflex.ps1
@@ -1,3 +1,3 @@
-$CompilerCmd = "java -cp '.\MJCompiler\lib\JFlex.jar' JFlex.Main -nobak -d '.\MJCompiler\src\rs\ac\bg\etf\pp1' '.\MJCompiler\spec\mjlexer.flex'"
+$CompilerCmd = "java -cp './MJCompiler/lib/jflex-1.4.3.jar' JFlex.Main -nobak -d './MJCompiler/src/rs/ac/bg/etf/pp1' './MJCompiler/spec/mjlexer.flex'"
 $CompilerCmd
 Invoke-Expression -Command $CompilerCmd
