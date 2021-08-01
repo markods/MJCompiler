b8695fee680efb03dbc12a4c3087661728c69c5d
Marko Stanojević <marko.d.stanojevic@gmail.com>
2021-08-01 15:15:22 +0200

    doradjena parser specifikacija

--- cup.ps1
+++ cup.ps1
@@ -1,5 +1,5 @@
 Push-Location "./MJCompiler/src" 
-$CompilerCmd = "java -cp '../lib/cup_v10k.jar' java_cup.Main -destdir './rs/ac/bg/etf/pp1' -parser 'MJParser' -ast rs.ac.bg.etf.pp1.ast -buildtree '../spec/mjparser.cup'"
+$CompilerCmd = "java -cp '../lib/cup_v10k.jar' java_cup.Main -destdir './rs/ac/bg/etf/pp1' -parser 'MJParser' '../spec/mjparser.cup'"
 $CompilerCmd
 Invoke-Expression -Command $CompilerCmd
 Pop-Location
